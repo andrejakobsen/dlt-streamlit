6f58657af501a5dbfa450c2198da7200b96dc8e9
Kristian André Jakobsen <k.andrejakobsen@gmail.com>
2024-06-01 17:27:00 +0200

    improve source builder by adding endpoints option

--- src/pages/1_🔧_Source_Builder.py
+++ src/pages/1_🔧_Source_Builder.py
@@ -9,69 +9,28 @@ st.title("Build a REST API Source")
 
 
 def source_builder(name: str):
-    # st.markdown(f"## Creating `{name}` Source")
-    st.markdown(f"#### 1. Configure the base client for `{name}`")
+    # st.markdown(f"## Creating '{name}' Source")
+    st.markdown(f"#### 1. Configure the base client for '{name}'")
     with st.expander("Client", expanded=True):
         client_config = get_client_config()
-    st.markdown(f"#### 2. Choose default configuration for `{name}`")
+    st.markdown(f"#### 2. Choose default configuration for '{name}'")
     with st.expander("Default Configuration"):
         resource_defaults_config = get_resource_standard_config()
-        left, right = st.columns(2)
-        params = {}
-        params_type = "JSON"
-        with left:
-            use_params = st.checkbox("Use parameters")
-        with right:
-            if use_params:
-                params_type = st.radio(
-                    "Choose",
-                    ("Key/value", "JSON"),
-                    horizontal=True,
-                    label_visibility="collapsed",
-                )
-
-        if use_params:
-            with left:
-                if params_type == "JSON":
-                    params = json.loads(
-                        st.text_area(
-                            "Parameters in JSON format", value='{\n\t"": ""\n}'
-                        )
-                    )
-                else:
-                    n_params = st.number_input(
-                        "Number of parameters", min_value=1, max_value=50
-                    )
-                    left_inner, right_inner = st.columns(2)
-
-                    for i in range(int(n_params)):
-                        with left_inner:
-                            key = st.text_input(f"Key {i+1}", placeholder="limit")
-                        with right_inner:
-                            value = st.text_input(f"Value {i+1}", placeholder="50")
-                            try:
-                                value = int(value)
-                            except ValueError:
-                                pass
-                            if value == "true":
-                                value = True
-                            elif value == "false":
-                                value = False
-                        params[key] = value
-            with right:
-                # st.markdown("Resulting parameters")
-                st.json(params)
-
-            # st.json(params)
-    st.markdown(f"#### 3. Configure the endpoints for `{name}`")
+        # st.json(params)
+    st.markdown(f"#### 3. Configure the endpoints for '{name}'")
     with st.expander("Endpoints"):
-        st.markdown("### Endpoints")
         n_resources = st.number_input("Number of endpoints", min_value=1, max_value=50)
+        resources_config = []
+
+        for i in range(int(n_resources)):
+            st.divider()
+            resources_config.append(get_resources_config(i))
     source_config = {
         "client": client_config,
         "resource_defaults": resource_defaults_config,
+        "resources": resources_config,
     }
-    st.markdown(f"#### 4. Make sure the conifguration for `{name}` looks as expected")
+    st.markdown(f"#### 4. Make sure the conifguration for '{name}' looks as expected")
     display_config = source_config
     display_config["client"]["auth"] = "HIDDEN"
     st.json(source_config)
@@ -79,7 +38,9 @@ def source_builder(name: str):
 
 def get_client_config():
     client_config = {
-        "base_url": st.text_input("Base URL", placeholder="https://api.example.com/v1"),
+        "base_url": st.text_input(
+            "Base URL", placeholder="https://api.github.com/repos/dlt-hub/dlt/"
+        ),
     }
     auth_type = st.selectbox(
         "Authentication",
@@ -97,19 +58,95 @@ def get_client_config():
     return client_config
 
 
-def get_resource_standard_config():
+def get_resource_standard_config(name: str = "Default"):
     resource_defaults_config = {}
+    if name != "Default":
+        resource_defaults_config["name"] = name
     col_1, col_2 = st.columns(2)
     with col_1:
         resource_defaults_config["write_disposition"] = st.radio(
-            "Write disposition", ("merge", "replace", "append"), horizontal=True
+            f"{name} write disposition", ("merge", "replace", "append"), horizontal=True
         )
     with col_2:
         resource_defaults_config["primary_key"] = st.text_input(
-            "Primary key", placeholder="id"
+            f"{name} primary key", placeholder="id"
         )
     resource_defaults_config["endpoint"] = {"params": {}}
+    left, right = st.columns(2)
+    params = {}
+    params_type = "JSON"
+    with left:
+        use_params = st.checkbox("Use parameters", key=f"checkbox_{name}")
+    with right:
+        if use_params:
+            params_type = st.radio(
+                "Choose",
+                ("Key/value", "JSON"),
+                horizontal=True,
+                label_visibility="collapsed",
+                key=f"radio_resource_{name}",
+            )
+
+    if use_params:
+        with right:
+            if params_type == "JSON":
+                params = json.loads(
+                    st.text_area(
+                        "Parameters in JSON format",
+                        value='{\n\t"": ""\n}',
+                        key=f"json_text_{name}",
+                    )
+                )
+            else:
+                n_params = st.number_input(
+                    "Number of parameters",
+                    min_value=1,
+                    max_value=50,
+                    key=f"number_{name}",
+                )
+                left_inner, right_inner = st.columns(2)
+
+                for i in range(int(n_params)):
+                    with left_inner:
+                        key = st.text_input(
+                            f"Key {i+1}", placeholder="limit", key=f"key_{i}_{name}"
+                        )
+                    with right_inner:
+                        value = st.text_input(
+                            f"Value {i+1}", placeholder="50", key=f"value_{i}_{name}"
+                        )
+                        try:
+                            value = int(value)
+                        except ValueError:
+                            pass
+                        if value == "true":
+                            value = True
+                        elif value == "false":
+                            value = False
+                    params[key] = value
+        with left:
+            st.markdown("Resulting parameters")
+            st.json(params)
+
     return resource_defaults_config
+
+
+def get_resources_config(endpoint_idx):
+    left, right = st.columns(2)
+    with left:
+        name = st.text_input(
+            f"Name of resource {endpoint_idx+1}", placeholder="Issues in dlt repo"
+        )
+    if name != "":
+        with right:
+            endpoint = st.text_input(f"Endpoint for {name}", placeholder="issues")
+        if endpoint != "":
+            use_default_params = st.checkbox(
+                f"Use custom configuration for '{name}'",
+            )
+            if use_default_params:
+                resource_config = get_resource_standard_config(name)
+    return {name: "some"}
 
 
 def _get_auth_input(auth_type):
@@ -137,5 +174,3 @@ if source_name != "":
     # create_source = st.button("Create Source")
     source_config = source_builder(source_name)
     st.button("Save source")
-
-# if create_source:
